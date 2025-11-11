d86c9b66b4f430fe7b15ce9ceea71041cd938ef9
DaniloPimenta10 <danilopimenta2006@gmail.com>
2025-11-11 08:17:44 -0300

    criacao da lista

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pickle
 
-arq = "entrada3.bin"
+arq = "entrada1.bin"
 
 with open(arq, "rb") as f:
     tipos = pickle.load(f)
     pontos = pickle.load(f)
     alunos = pickle.load(f)
 
+def cria_lista(alunos):
+    atividades = []
+    for matricula in alunos:
+        for i in range(len(alunos[matricula][1])):
+            atividades.append((matricula, i))
+    print(atividades)
+
+cria_lista(alunos)
 
